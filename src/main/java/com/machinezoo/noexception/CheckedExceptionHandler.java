301dc91858c7c2f20ceb8493a457a17e1d70e4be
Robert Vazan <robvcode@gmail.com>
2017-05-20 21:54:33 +0200

    Primitive suppliers in CheckedExceptionHandler

--- src/main/java/com/machinezoo/noexception/CheckedExceptionHandler.java
+++ src/main/java/com/machinezoo/noexception/CheckedExceptionHandler.java
@@ -7,9 +7,9 @@ import lombok.*;
 public abstract class CheckedExceptionHandler {
 	public abstract void handle(Throwable exception);
 	public final Runnable runnable(ThrowingRunnable runnable) {
-		return new SneakyRunnable(runnable);
+		return new CheckedRunnable(runnable);
 	}
-	@RequiredArgsConstructor private final class SneakyRunnable implements Runnable {
+	@RequiredArgsConstructor private final class CheckedRunnable implements Runnable {
 		private final ThrowingRunnable runnable;
 		@Override public void run() {
 			try {
@@ -20,9 +20,9 @@ public abstract class CheckedExceptionHandler {
 		}
 	}
 	public final <T> Supplier<T> supplier(ThrowingSupplier<T> supplier) {
-		return new SneakySupplier<>(supplier);
+		return new CheckedSupplier<T>(supplier);
 	}
-	@RequiredArgsConstructor private final class SneakySupplier<T> implements Supplier<T> {
+	@RequiredArgsConstructor private final class CheckedSupplier<T> implements Supplier<T> {
 		private final ThrowingSupplier<T> supplier;
 		@Override public T get() {
 			try {
@@ -30,6 +30,62 @@ public abstract class CheckedExceptionHandler {
 			} catch (Throwable exception) {
 				handle(exception);
 				return null;
+			}
+		}
+	}
+	public final IntSupplier fromIntSupplier(ThrowingIntSupplier supplier) {
+		return new CheckedIntSupplier(supplier);
+	}
+	@RequiredArgsConstructor private final class CheckedIntSupplier implements IntSupplier {
+		private final ThrowingIntSupplier supplier;
+		@Override public int getAsInt() {
+			try {
+				return supplier.getAsInt();
+			} catch (Throwable exception) {
+				handle(exception);
+				return 0;
+			}
+		}
+	}
+	public final LongSupplier fromLongSupplier(ThrowingLongSupplier supplier) {
+		return new CheckedLongSupplier(supplier);
+	}
+	@RequiredArgsConstructor private final class CheckedLongSupplier implements LongSupplier {
+		private final ThrowingLongSupplier supplier;
+		@Override public long getAsLong() {
+			try {
+				return supplier.getAsLong();
+			} catch (Throwable exception) {
+				handle(exception);
+				return 0;
+			}
+		}
+	}
+	public final DoubleSupplier fromDoubleSupplier(ThrowingDoubleSupplier supplier) {
+		return new CheckedDoubleSupplier(supplier);
+	}
+	@RequiredArgsConstructor private final class CheckedDoubleSupplier implements DoubleSupplier {
+		private final ThrowingDoubleSupplier supplier;
+		@Override public double getAsDouble() {
+			try {
+				return supplier.getAsDouble();
+			} catch (Throwable exception) {
+				handle(exception);
+				return 0;
+			}
+		}
+	}
+	public final BooleanSupplier fromBooleanSupplier(ThrowingBooleanSupplier supplier) {
+		return new CheckedBooleanSupplier(supplier);
+	}
+	@RequiredArgsConstructor private final class CheckedBooleanSupplier implements BooleanSupplier {
+		private final ThrowingBooleanSupplier supplier;
+		@Override public boolean getAsBoolean() {
+			try {
+				return supplier.getAsBoolean();
+			} catch (Throwable exception) {
+				handle(exception);
+				return false;
 			}
 		}
 	}
