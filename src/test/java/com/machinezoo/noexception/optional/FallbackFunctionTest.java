e59a540ac6279142a4bd2dfb313bade5d88d3855
Robert Vazan <robvcode@gmail.com>
2017-05-26 00:22:38 +0200

    Test for FallbackFunction

---
+++ src/test/java/com/machinezoo/noexception/optional/FallbackFunctionTest.java
@@ -0,0 +1,29 @@
+// Part of NoException: https://noexception.machinezoo.com
+package com.machinezoo.noexception.optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+import java.util.function.*;
+import org.junit.*;
+
+public class FallbackFunctionTest {
+	@Test public void full() {
+		@SuppressWarnings("unchecked") OptionalFunction<String, String> full = mock(OptionalFunction.class);
+		when(full.apply("input")).thenReturn(Optional.of("value"));
+		@SuppressWarnings("unchecked") Supplier<String> fallback = mock(Supplier.class);
+		when(fallback.get()).thenReturn("fallback");
+		assertEquals("value", new FallbackFunction<String, String>(full, fallback).apply("input"));
+		verify(full, only()).apply("input");
+		verifyNoMoreInteractions(fallback);
+	}
+	@Test public void empty() {
+		@SuppressWarnings("unchecked") OptionalFunction<String, String> empty = mock(OptionalFunction.class);
+		when(empty.apply("input")).thenReturn(Optional.empty());
+		@SuppressWarnings("unchecked") Supplier<String> fallback = mock(Supplier.class);
+		when(fallback.get()).thenReturn("fallback");
+		assertEquals("fallback", new FallbackFunction<String, String>(empty, fallback).apply("input"));
+		verify(empty, only()).apply("input");
+		verify(fallback, only()).get();
+	}
+}
