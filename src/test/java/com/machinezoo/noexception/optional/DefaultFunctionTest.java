95799087d237415ada2a702a79a4fae244e61361
Robert Vazan <robvcode@gmail.com>
2017-05-25 20:50:47 +0200

    Test for DefaultFunction

---
+++ src/test/java/com/machinezoo/noexception/optional/DefaultFunctionTest.java
@@ -0,0 +1,22 @@
+// Part of NoException: https://noexception.machinezoo.com
+package com.machinezoo.noexception.optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+import org.junit.*;
+
+public class DefaultFunctionTest {
+    @Test public void full() {
+        @SuppressWarnings("unchecked") OptionalFunction<String, String> full = mock(OptionalFunction.class);
+        when(full.apply("input")).thenReturn(Optional.of("value"));
+        assertEquals("value", new DefaultFunction<String, String>(full, "default").apply("input"));
+        verify(full, only()).apply("input");
+    }
+    @Test public void empty() {
+        @SuppressWarnings("unchecked") OptionalFunction<String, String> empty = mock(OptionalFunction.class);
+        when(empty.apply("input")).thenReturn(Optional.empty());
+        assertEquals("default", new DefaultFunction<String, String>(empty, "default").apply("input"));
+        verify(empty, only()).apply("input");
+    }
+}
