b705efa315680fd660cb6b031af66a872c615c1f
Robert Vazan <robvcode@gmail.com>
2017-05-26 18:52:40 +0200

    Tests for ExceptionHandler.runnable/function

--- src/test/java/com/machinezoo/noexception/ExceptionHandlerTest.java
+++ src/test/java/com/machinezoo/noexception/ExceptionHandlerTest.java
@@ -1,10 +1,81 @@
 package com.machinezoo.noexception;
 
+import static org.hamcrest.core.IsInstanceOf.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+import java.util.function.*;
 import org.junit.*;
+import lombok.*;
 
 public class ExceptionHandlerTest {
-	@Test public void example() {
-		assertTrue(true);
+	@Test public void runnable_complete() {
+		ExceptionCollector collector = new ExceptionCollector(true);
+		Runnable lambda = mock(Runnable.class);
+		collector.runnable(lambda).run();
+		verify(lambda, only()).run();
+		assertTrue(collector.empty());
+	}
+	@Test public void runnable_swallowException() {
+		ExceptionCollector collector = new ExceptionCollector(true);
+		collector.runnable(() -> {
+			throw new NumberFormatException();
+		}).run();
+		assertThat(collector.single(), instanceOf(NumberFormatException.class));
+	}
+	@Test public void runnable_passException() {
+		ExceptionCollector collector = new ExceptionCollector(false);
+		try {
+			collector.runnable(() -> {
+				throw new NumberFormatException();
+			}).run();
+			fail();
+		} catch (NumberFormatException e) {
+		}
+		assertThat(collector.single(), instanceOf(NumberFormatException.class));
+	}
+	@Test public void function_complete() {
+		ExceptionCollector collector = new ExceptionCollector(true);
+		@SuppressWarnings("unchecked") Function<String, String> lambda = mock(Function.class);
+		when(lambda.apply("input")).thenReturn("value");
+		assertEquals(Optional.of("value"), collector.function(lambda).apply("input"));
+		verify(lambda, only()).apply("input");
+		assertTrue(collector.empty());
+	}
+	@Test public void function_swallowException() {
+		ExceptionCollector collector = new ExceptionCollector(true);
+		assertEquals(Optional.empty(), collector.function(x -> {
+			throw new NumberFormatException();
+		}).apply("input"));
+		assertThat(collector.single(), instanceOf(NumberFormatException.class));
+	}
+	@Test public void function_passException() {
+		ExceptionCollector collector = new ExceptionCollector(false);
+		try {
+			collector.function(x -> {
+				throw new NumberFormatException();
+			}).apply("input");
+			fail();
+		} catch (NumberFormatException e) {
+		}
+		assertThat(collector.single(), instanceOf(NumberFormatException.class));
+	}
+	@RequiredArgsConstructor private static class ExceptionCollector extends ExceptionHandler {
+		final boolean swallow;
+		final List<Throwable> collected = new ArrayList<>();
+		public int size() {
+			return collected.size();
+		}
+		public boolean empty() {
+			return size() == 0;
+		}
+		public Throwable single() {
+			assertEquals(1, size());
+			return collected.get(0);
+		}
+		@Override public boolean handle(Throwable exception) {
+			collected.add(exception);
+			return swallow;
+		}
 	}
 }
