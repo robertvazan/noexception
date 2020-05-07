a26fe8e87048ecb0bd5d4239bf0153d26eeba98d
Robert Vazan <robvcode@gmail.com>
2020-05-07 18:00:14 +0000

    Note about CompletableFuture

--- src/main/java/com/machinezoo/noexception/package-info.java
+++ src/main/java/com/machinezoo/noexception/package-info.java
@@ -1,4 +1,15 @@
 // Part of NoException: https://noexception.machinezoo.com
+/*
+ * It is tempting to add CompletableFuture wrapper. For example, logging wrapper would be quite useful to prevent swallowing of exceptions.
+ * There is however a number of arguments against this:
+ * - It would only work in ExceptionHandler, not CheckedExceptionHandler, because exceptions from CompletableFuture are already wrapped.
+ * - The wrapper would produce CompletableFuture<Optional<T>>. There's no convenient way to add orElse() method to it.
+ * - Futures are not functional interfaces like the rest. They can be wrapped, but it costs a lot of complexity and it doesn't fit the API.
+ * - There is a duality between CompletableFuture and CompletionStage, which further increases complexity on both API and implementation level.
+ * - Futures are not used frequently, at least not yet. They are a low priority problem.
+ * 
+ * Wrappers for ExecutorService would have similar problems.
+ */
 /**
  * Classes implementing all NoException features.
  * Class {@link com.machinezoo.noexception.Exceptions} is the entry point to NoException functionality.
