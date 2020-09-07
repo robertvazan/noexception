47e15bab6ed214fc0cb3a9a13e54ac022b268d60
Robert Važan <robert.vazan@protonmail.com>
2020-09-07 03:00:03 +0000

    Added andThen and andFinally to CloseableScope

--- src/main/java/com/machinezoo/noexception/CloseableScope.java
+++ src/main/java/com/machinezoo/noexception/CloseableScope.java
@@ -20,4 +20,46 @@ public interface CloseableScope extends AutoCloseable {
 	 * This is a specialization of {@link AutoCloseable#close()} that throws only unchecked exceptions.
 	 */
 	void close();
+	/*
+	 * We could also take CloseableScope as a parameter, which would be more consistent with andThen methods in Consumer and such,
+	 * but Runnable is more common and CloseableScopes are unlikely to need to be combined in practice.
+	 * We don't want to overload on Runnable and CloseableScope, because that would create ambiguity for lambdas.
+	 */
+	/**
+	 * Creates extended {@code CloseableScope} that additionally runs specified {@code action} unless exception is thrown.
+	 * This is useful for quickly creating outer scopes that add extra operations to some inner scope.
+	 * If {@link #close()} throws, {@code action} does not run.
+	 * 
+	 * @param action
+	 *            the operation to perform after {@link #close()} is called
+	 * @return new {@code CloseableScope} that, when closed, first closes this {@code CloseableScope} and then executes {@code action}
+	 * 
+	 * @see #andFinally(Runnable)
+	 */
+	default CloseableScope andThen(Runnable action) {
+		return () -> {
+			close();
+			action.run();
+		};
+	}
+	/**
+	 * Creates extended {@code CloseableScope} that additionally runs specified {@code action} regardless of exceptions.
+	 * This is useful for quickly creating outer scopes that add extra operations to some inner scope.
+	 * If {@link #close()} throws, {@code action} runs anyway as if in {@code finally} block.
+	 * 
+	 * @param action
+	 *            the operation to perform after {@link #close()} is called
+	 * @return new {@code CloseableScope} that, when closed, first closes this {@code CloseableScope} and then executes {@code action}
+	 * 
+	 * @see #andThen(Runnable)
+	 */
+	default CloseableScope andFinally(Runnable action) {
+		return () -> {
+			try {
+				close();
+			} finally {
+				action.run();
+			}
+		};
+	}
 }
