f852e6a103b64f9a26ad2f40c901ff60332ca46d
Robert Važan <robert.vazan@tutanota.com>
2022-08-04 19:08:50 +0000

    Updated project: build PRs, upgraded stagean, note about generated files

--- scripts/configure.py
+++ scripts/configure.py
@@ -2,11 +2,10 @@
 
 # We are assuming that project-config is available in sibling directory.
 # Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
-
-from java import *
+import pathlib
+project_directory = lambda: pathlib.Path(__file__).parent.parent
+config_directory = lambda: project_directory().parent/'project-config'
+exec((config_directory()/'src'/'java.py').read_text())
 
 project_script_path = __file__
 repository_name = lambda: 'noexception'
@@ -30,4 +29,4 @@ def badges():
     standard_badges()
     print('[![Mentioned in Awesome Java 8](https://awesome.re/mentioned-badge.svg)](https://github.com/tedyoung/awesome-java8)')
 
-generate(globals())
+generate()
